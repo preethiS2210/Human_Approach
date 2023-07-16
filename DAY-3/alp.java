40cd094b660c4d7bc8553c5ca444646d760df957
preethiS2210 <139195145+preethiS2210@users.noreply.github.com>
2023-07-16 13:53:42 +0530

    Add files via upload

---
+++ DAY-3/alp.java
@@ -0,0 +1,16 @@
+import java.io.*;
+public class alp
+{
+	public static void main(String[] args)
+	{
+		int n=5,alphabet=65;
+		for(int i=0;i<=n;i++)
+		{
+			for(int j=0;j<=i;j++)
+			{
+				System.out.print((char)(alphabet + j));
+			}
+				System.out.println();
+		}
+	}
+}
